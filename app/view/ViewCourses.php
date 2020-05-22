00909a205804aad76c4e64395dad91dd498a4891
mohamed1527 <mohamedayman008@gmail.com>
2020-05-22 23:32:15 +0200

    new page when press on section price

---
+++ app/view/ViewCourses.php
@@ -0,0 +1,26 @@
+<?php
+require_once("View3.php");
+class ViewCourses extends View3{
+	
+    public function coursesdescription(){
+        $id = $_GET['id'];
+        $str = "";
+        $courses = $this->model->getCourses();
+    
+            foreach($courses[$id]->getSections() as $section){
+
+                $str = $str . '<div class="card mb-3">';
+                $str .= '<img src="images/literature.jpg" class="card-img-top" alt="Error">';
+                $str .='<div class="card-body">';
+                $str .='<h2 class="card-title">'.$section->getSectioncost().'</h2>';
+                $str .='<a href="#" class="btn btn-primary">Buy Now</a>';
+                $str .='<p class="card-text">'.$section->getSectionTime().'</p>';
+                $str .='</div>';
+                $str .='</div>';
+            }
+        
+        return $str;
+    }
+}
+?>
+                
