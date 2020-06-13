0376ce886a5230c604f49250134313d64be30f96
mohamed1527 <mohamedayman008@gmail.com>
2020-06-13 11:36:20 +0200

    new

--- public/signup.php
+++ public/signup.php
@@ -18,6 +18,9 @@ $controller = new UserController($model);
     <title>Online Study</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <script src="https://www.google.com/recaptcha/api.js"></script>
+    
+    
     
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
@@ -122,9 +125,11 @@ function count3(){
 /* Style the input container */
 .input-container {
   display: flex;
-  width: 50%;
-  margin-bottom: 15px;
-  margin-left: 300px;
+  width: 40%;
+  margin-bottom: 40px;
+  margin-left: 430px;
+  padding-bottom:10px; 
+  margin-bottom: 10px;
 }
 
 /* Style the form icons */
@@ -154,15 +159,30 @@ function count3(){
   padding: 15px 20px;
   border: none;
   cursor: pointer;
-  width: 50%;
+  width: 40%;
   opacity: 0.9;
-  margin-left: 300px;
+  margin-left: 430px;
 }
 
 .btn:hover {
   opacity: 1;
 }
+.sml{
+margin-top: 0px;
+margin-left: 455px;
+padding-top: 0px;
+
+}
+.invalid {
+    border: 2px solid red !important;
+}
+.jumbotron{
+width: 50%;
+text-align: center;
+}
+
     </style>
+    
     
     
   </head>
@@ -192,41 +212,41 @@ function count3(){
   <h2 style="margin-left: 570px;color:#00043c ">Sign Up</h2>
   <div class="input-container">
     <i class="fa fa-user icon"></i>
-    <input class="input-field" type="text" placeholder="First Name" name="firstname" id="firstname" onchange="count1();">
+    <input class="input-field" id="inputFirstName" type="text" placeholder="First Name" name="firstname" id="firstname" minlength="3" maxlength="20" required>
   </div>
   <div class="input-container">
     <i class="fa fa-user icon"></i>
-    <input class="input-field" type="text" placeholder="Last Name" name="lastname" id="lastname" onchange="count2();">
+    <input class="input-field" id="inputLastName" type="text" placeholder="Last Name" name="lastname" id="lastname" minlength="3" maxlength="20" required >
   </div>
  
   <div class="input-container">
     <i class="fa fa-envelope icon"></i>
-    <input class="input-field" type="email" placeholder="Email" name="email">
+    <input class="input-field" id="inputEmail"  type="email" placeholder="Email" name="email" id="email" required>
   </div>
 
   <div class="input-container">
     <i class="fa fa-key icon"></i>
-    <input class="input-field" type="password" placeholder="Password" id="password" name="password" onchange="count();" onkeyup="matching()">
+    <input class="input-field" id="inputPassword" type="password" placeholder="Password" id="password" name="password" onchange="count();" >
   </div>
 <div class="input-container">
     <i class="fa fa-key icon"></i>
-    <input class="input-field" type="password" placeholder="Re-type Password" id="repeatpassword" name="repeatpassword" onchange="count();" onkeyup="matching()">
+    <input class="input-field" id="inputConfirmPassword" type="password" placeholder="Re-type Password" id="repeatpassword" name="repeatpassword">
     <br>
     <div id="msg" > </div>
   </div>
   <div class="input-container">
     <i class="fa fa-phone icon"></i>
-    <input class="input-field" type="text" placeholder="Mobile number" name="phone" id="phone" onchange="count3();">
+    <input class="input-field"  onkeydown="javascript: return event.keyCode === 8 || event.keyCode === 46 ? true : !isNaN(Number(event.key))"id="Phone" type="phone" placeholder="Mobile number" maxlength="11" min="0" max="9">
   </div>
   <div class="input-container">
     <i class="fa fa-home icon"></i>
-    <input class="input-field" type="text" placeholder="Address" name="Address">
+    <input class="input-field" type="text" placeholder="Address" name="Address" required>
   </div>
   <div class="input-container">
     <i class="fa fa-image icon"></i>
     <input type="file" name="img" accept="image/gif, image/jpeg, image/png" /><br>
   </div>  
-  <button type="submit" id="register" name="register" class="btn">Register</button>
+  <button type="submit" id="submitSignUp" id="register" name="register" class="btn">Register</button>
 </form>
 
 
@@ -245,6 +265,6 @@ function count3(){
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="../lib/js/google-map.js"></script>
   <script src="../lib/js/main.js"></script>
-    
+  <script src="../lib/js/signup.js"></script>   
   </body>
   </html>
